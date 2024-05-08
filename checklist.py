9609e231dc72f80239726aa62ecaa07a1d546b78
Austin Chen <akrolsmir@gmail.com>
2024-05-08 17:59:09 -0600

    Add help text + a couple more examples

--- checklist.py
+++ checklist.py
@@ -41,9 +41,9 @@ with col1:
             "Honesty and accuracy": honesty_prompt,
             # "Respond with response to honesty and accuracy for testing",
             # "Context for unfamiliar items": "Respond with response to context for unfamiliar items for testing",
-            # "Relevance to the project": "Respond with response to relevance to the project for testing",
+            "Relevance to the project": "Respond with response to relevance to the project for testing",
             # "Past projects": "Respond with response to past projects for testing",
-            # "Past funding": "Respond with response to past funding for testing",
+            "Past funding": "Respond with response to past funding for testing",
             # "Bragging": "Respond with response to bragging for testing",
         }
 
@@ -54,11 +54,11 @@ with col1:
             print(trackRecord, rubric_prompts[rubric])
 
 
-def rubric_item(title, score, response="LLM response goes here. 5/10"):
+def rubric_item(title, score, help="", response="LLM response goes here. 5/10"):
     # Override the score from the response, which ends with a score like ". 5/10"
     score = response.split(".")[-1].strip()
 
-    st.metric(title, score)
+    st.metric(title, score, help=help)
     with st.expander("Ulysses says:"):
         st.write(response)
 
@@ -68,6 +68,7 @@ with col2:
     rubric_item(
         "Honesty and accuracy",
         "6/10",
+        "Did the track record section look like an honest and accurate self-assessment, rather than trying to “sell” the grantmakers on something?",
         response=st.session_state.llm_responses.get("Honesty and accuracy", ""),
     )
     # rubric_item(
@@ -75,21 +76,23 @@ with col2:
     #     "3/10",
     #     response=st.session_state.llm_responses.get("Context for unfamiliar items", ""),
     # )
-    # rubric_item(
-    #     "Relevance to the project",
-    #     "8/10",
-    #     response=st.session_state.llm_responses.get("Relevance to the project", ""),
-    # )
+    rubric_item(
+        "Relevance to the project",
+        "8/10",
+        "If the applicant has salient experiences to the project they’re applying for, are they clearly mentioned and highlighted? If the applicant does NOT have important relevant experiences for the project they’re applying for, is it mentioned upfront rather than hidden?",
+        response=st.session_state.llm_responses.get("Relevance to the project", ""),
+    )
     # rubric_item(
     #     "Past projects",
     #     "4/10",
     #     response=st.session_state.llm_responses.get("Past projects", ""),
     # )
-    # rubric_item(
-    #     "Past funding",
-    #     "2/10",
-    #     response=st.session_state.llm_responses.get("Past funding", ""),
-    # )
+    rubric_item(
+        "Past funding",
+        "2/10",
+        "If the applicant has gotten funding from us in the past, did the application mention the project in a way that’s easy for us to identify?",
+        response=st.session_state.llm_responses.get("Past funding", ""),
+    )
     # rubric_item(
     #     "Bragging",
     #     "8/10",

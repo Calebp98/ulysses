fc17d1743338ef6afcb4195d89caea7a7d7f855e
Caleb Parikh <parikhcaleb@gmail.com>
2024-05-09 23:11:38 -0600

    updated using pipreqs

--- checklist.py
+++ checklist.py
@@ -15,7 +15,6 @@ if "llm_responses" not in st.session_state:
     st.session_state.llm_responses = {}
 
 
-
 async def generate_response(user_response, rubric_prompt):
     async with aiohttp.ClientSession() as session:
         messages = [
@@ -64,12 +63,10 @@ def rubric_item(title, score, help="", response="LLM response goes here. 5/10"):
     # Override the score from the response, which ends with a score like ". 5/10"
     score = response.split(".")[-1].strip()
 
-
     st.metric(title, score, help=help)
 
     with st.expander("Ulysses says:"):
         st.write(response)
-
 
 
 # Get the list of subdirectories in the "prompts" directory
@@ -103,4 +100,3 @@ for section_dir in prompt_dirs:
                             f"{section_dir}_{rubric_title}", ""
                         ),
                     )
-
